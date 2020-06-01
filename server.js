dd795918a3038bbbc60d4316760584d0f36bd3ed
Sarisha Das <sarishadas.810@gmail.com>
2020-06-02 05:24:43 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -10,11 +10,19 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.json({ msg: "Hello" }));
-
 //Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/books", require("./routes/books"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`Server running on port${PORT}`));
