95bb33b0afaf3bebe2b6b24bdb2e244ec66bb71f
Sarisha Das <sarishadas.810@gmail.com>
2020-06-18 02:33:43 +0530

    Material UI2

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/uploads", express.static("uploads"));
 if (process.env.NODE_ENV === "production") {
   //Set static folder
   app.use(express.static("client/build"));
-  app.use("/uploads", express.static("uploads"));
+
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
